f8ae5a59088bf26a930958a1adcd905ed4104636
Comma Device <device@comma.ai>
2020-10-15 15:03:45 -0700

    Restore Tesla fingerprint force option

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -8,6 +8,7 @@ from selfdrive.car.fw_versions import get_fw_versions, match_fw_to_car
 from selfdrive.swaglog import cloudlog
 import cereal.messaging as messaging
 from selfdrive.car import gen_empty_fingerprint
+from selfdrive.car.tesla.readconfig import CarSettings
 
 from cereal import car, log
 EventName = car.CarEvent.EventName
@@ -147,6 +148,11 @@ def fingerprint(logcan, sendcan, has_relay):
           # fingerprint done
           car_fingerprint = candidate_cars[b][0]
 
+    if (car_fingerprint is None) and CarSettings().forceFingerprintTesla:
+          print ("Fingerprinting Failed: Returning Tesla (based on branch)")
+          car_fingerprint = "TESLA MODEL S"
+          vin = "TESLAFAKEVIN12345"
+
     # bail if no cars left or we've been waiting for more than 2s
     failed = all(len(cc) == 0 for cc in candidate_cars.values()) or frame > 200
     succeeded = car_fingerprint is not None
@@ -170,7 +176,17 @@ def fingerprint(logcan, sendcan, has_relay):
 
 
 def get_car(logcan, sendcan, has_relay=False):
-  candidate, fingerprints, vin, car_fw, source = fingerprint(logcan, sendcan, has_relay)
+  if CarSettings().forceFingerprintTesla:
+    candidate="TESLA MODEL S"
+    fingerprints=["","",""]
+    vin="TESLAFORCED123456"
+    #BB
+    car_fw = []
+    source=car.CarParams.FingerprintSource.fixed
+    cloudlog.warning("VIN %s", vin)
+    Params().put("CarVin", vin)
+  else:
+    candidate, fingerprints, vin, car_fw, source = fingerprint(logcan, sendcan, has_relay)
 
   if candidate is None:
     cloudlog.warning("car doesn't match any fingerprints: %r", fingerprints)
